bd6780c937593ae56488a0c0f46587f227195d90
kento <kento.kawahara@rakuten-sec.co.jp>
2021-09-07 16:38:02 +0900

    3-7答案

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
 });
+//問3
+Route::get('XXX', 'AAAController@bbb');
+
+//問4
+Route::get('admin/profile/create', 'Admin\ProfileController@add');
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+
+?>
